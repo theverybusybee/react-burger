224c87aeb0dfbb27c7cf91ee048119c3c73e72d6
theverybusybee <v_antonchik@mail.ru>
2022-09-12 15:09:21 +0300

    actions and reducers added

--- src/services/actions/actions.js
+++ src/services/actions/actions.js
@@ -1,3 +1,5 @@
+import { fetchIngredients } from "../../utils/fetchOrderData";
+
 /* api reducer */
 export const FETCH_API_REQUEST = 'FETCH_API_REQUEST'
 export const FETCH_API_SUCCESS = "FETCH_API_SUCCESS";
@@ -5,6 +7,39 @@ export const FETCH_API_ERROR = "FETCH_API_ERROR";
 
 /* order reducer */
 export const SET_INGREDIENTS = "SET_INGREDIENTS";
-export const SET_BUNS = "SET_BUNS";
 export const ADD = "ADD";
 export const RESET = "RESET";
+
+
+export const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST';
+export const GET_INGREDIENTS_SUCCESS = 'GET_INGREDIENTS_SUCCESS';
+export const GET_INGREDIENTS_FAILED = 'GET_INGREDIENTS_FAILED';
+
+export const SET_CURRENT_CONSTRUCTOR_INGREDIENTS = 'SET_CURRENT_CONSTRUCTOR_INGREDIENTS';
+
+export const SET_BUNS = 'SET_BUNS';
+
+export const SET_MODAL_INGREDIENT = 'SET_MODAL_INGREDIENT';
+export const DELETE_MODAL_INGREDIENT = 'SET_MODAL_INGREDIENT';
+
+export function getIngredients() {
+  return function(dispatch) {
+   dispatch({ type: GET_INGREDIENTS_REQUEST });
+      fetchIngredients()
+        .then((res) => {
+          if (res && res.success) {
+            dispatch({
+              type: GET_INGREDIENTS_SUCCESS,
+              payload: res.data,
+            });
+          } else {
+            dispatch({ type: GET_INGREDIENTS_FAILED });
+          }
+        })
+        .catch(() =>
+          dispatch({
+            type: GET_INGREDIENTS_FAILED,
+          })
+        );
+    };
+  };
---
+++ src/services/reducers/reducer.js
@@ -0,0 +1,60 @@
+import { combineReducers } from "redux";
+
+import {
+  GET_INGREDIENTS_REQUEST,
+  GET_INGREDIENTS_SUCCESS,
+  GET_INGREDIENTS_FAILED,
+  SET_CURRENT_CONSTRUCTOR_INGREDIENTS,
+  SET_BUNS,
+  SET_MODAL_INGREDIENT,
+  DELETE_MODAL_INGREDIENT
+} from "../actions/order";
+
+const initialState = {
+  allIngredients: [],
+  allIngredientsRequest: false,
+  allIngredientsFailed: false,
+
+  currentConstructorIngredients: [],
+
+  currentModalIngredient: {},
+
+  createdOrder: {},
+
+  buns: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_INGREDIENTS_REQUEST:
+      return { ...state, allIngredientsRequest: true };
+    case GET_INGREDIENTS_SUCCESS:
+      return {
+        ...state,
+        allIngredients: action.payload,
+        allIngredientsRequest: false,
+        allIngredientsFailed: false,
+      };
+    case GET_INGREDIENTS_FAILED:
+      return { ...state, allIngredientsFailed: true };
+
+    case SET_CURRENT_CONSTRUCTOR_INGREDIENTS:
+      return { ...state, currentConstructorIngredients: action.payload };
+
+    case SET_BUNS:
+      return { ...state, buns: action.payload };
+
+      case SET_MODAL_INGREDIENT:
+      return { ...state, currentModalIngredient: action.payload };
+
+      case DELETE_MODAL_INGREDIENT: 
+      return { ...state, currentModalIngredient: {} }
+
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  order: reducer,
+});
