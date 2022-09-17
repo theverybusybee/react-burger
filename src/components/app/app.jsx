b6f647bdfa25da5427c023c380a006ea6d69150a
theverybusybee <v_antonchik@mail.ru>
2022-09-17 03:08:11 +0300

    ApiLoader component added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,31 +5,25 @@ import BurgerConstructor from "../burger-constructor/burger-constructor";
 import useFetchIngredients from "../../services/hooks/useFetchIngredients";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import ApiLoader from "../api-loader/api-loader";
 
 function App() {
   const { hasError, isLoading, data } = useFetchIngredients();
-
-  if (hasError || isLoading || !data.length) {
-    return (
-      <div className={AppStyle.main}>
-        <AppHeader />
-        <div className={AppStyle.loading}>
-          <p className="text text_type_main-large text_color_inactive">
-            Загрузка<span className={AppStyle.dotFlashing}></span>
-          </p>
-        </div>
-      </div>
-    );
-  } else
-    return (
-      <div className={AppStyle.main}>
-        <AppHeader />
+  return hasError || isLoading || !data.length ? (
+    <ApiLoader />
+  ) : (
+    <div className={AppStyle.main}>
+      <AppHeader />
+      {isLoading ? (
+        <ApiLoader />
+      ) : (
         <DndProvider backend={HTML5Backend}>
           <BurgerIngredients />
           <BurgerConstructor />
         </DndProvider>
-      </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default App;
