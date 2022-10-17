c0f6ec41bdfda2288925204bd50f3e243c33f28f
theverybusybee <v_antonchik@mail.ru>
2022-10-18 00:04:11 +0300

    ProtectedRoute component added

--- src/components/protected-route/protected-route.jsx
+++ src/components/protected-route/protected-route.jsx
@@ -1,16 +1,22 @@
 import { Redirect, Route, useLocation } from "react-router-dom";
 
-function ProtectedRoute({ onlyUnAuth = false, user, children, ...rest }) {
+export default function ProtectedRoute({
+  anonymous = false,
+  isAuth,
+  children,
+  ...rest
+}) {
+  const location = useLocation();
 
-  if (onlyUnAuth && user) {
+  if (anonymous && isAuth) {
     return <Redirect to="/" />;
   }
 
-  if (!onlyUnAuth && !user) {
-    return <Redirect to={{ pathname: "/login" }} />;
+  if (!anonymous && !isAuth) {
+    return (
+      <Redirect to={{ pathname: "/login", from: location}} />
+    );
   }
 
   return <Route {...rest}>{children}</Route>;
 }
-
-export default ProtectedRoute;
