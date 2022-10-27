bac36b84fa45b0c270ac42c3ad3faf33f3acfc40
theverybusybee <v_antonchik@mail.ru>
2022-10-27 08:59:58 +0300

    FeedDetailsIngredient functionality added

--- src/components/feed-details-ingredient/feed-details-ingredient.jsx
+++ src/components/feed-details-ingredient/feed-details-ingredient.jsx
@@ -1,32 +1,73 @@
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useState, useEffect, memo, useCallback } from "react";
 import ingredientStyles from "./feed-details-ingredient.module.css";
 
 function FeedDetailsIngredient({ data }) {
-  return (
-    <li className={ingredientStyles.containerItem}>
-      <div className={ingredientStyles.ingredient}>
-        <div className={ingredientStyles.ingredientBackground}>
-          <img
-            className={ingredientStyles.ingredientImage}
-            src={data.image}
-            alt="image1"
-          />
-        </div>
-      </div>
-      <p
-        className={`${ingredientStyles.ingredientName} text text_type_main-default`}
-      >
-        {data.name}
-      </p>
-      <p
-        className={`${ingredientStyles.amountAndCostContainer} text text_type_digits-default`}
-      >
-        <span className={ingredientStyles.amount}>2</span> x{" "}
-        <span className={ingredientStyles.price}>200</span>
-        <CurrencyIcon className={ingredientStyles.currency} type="primary" />
-      </p>
-    </li>
+  const [orderState, setOrderState] = useState({
+    ingredients: [],
+    price: 0,
+    reducedIngredients: [],
+  });
+
+  const { ingredients, reducedIngredients } = orderState;
+
+  const qty = useCallback(
+    (currentIngredientId) => {
+      if (ingredients.length) {
+        return ingredients.filter(
+          (ingredient) => ingredient._id === currentIngredientId
+        ).length;
+      }
+    },
+    [ingredients]
   );
+
+  useEffect(() => {
+    if (data.length) {
+      setOrderState({
+        ...orderState,
+        ingredients: data,
+        price: data.map((el) => el.price).reduce((a, b) => a + b),
+        reducedIngredients: Array.from(new Set(data)),
+      });
+    }
+  }, [data]);
+
+  return reducedIngredients.length
+    ? reducedIngredients.map((ingredient) => {
+        return (
+          <li className={ingredientStyles.containerItem}>
+            <div className={ingredientStyles.ingredient}>
+              <div className={ingredientStyles.ingredientBackground}>
+                <img
+                  className={ingredientStyles.ingredientImage}
+                  src={ingredient.image}
+                  alt={ingredient.name}
+                />
+              </div>
+            </div>
+            <p
+              className={`${ingredientStyles.ingredientName} text text_type_main-default`}
+            >
+              {ingredient.name}
+            </p>
+            <p
+              className={`${ingredientStyles.amountAndCostContainer} text text_type_digits-default`}
+            >
+              <span className={ingredientStyles.amount}>
+                {qty(ingredient._id)}
+              </span>{" "}
+              x{" "}
+              <span className={ingredientStyles.price}>{ingredient.price}</span>
+              <CurrencyIcon
+                className={ingredientStyles.currency}
+                type="primary"
+              />
+            </p>
+          </li>
+        );
+      })
+    : null;
 }
 
-export default FeedDetailsIngredient;
+export default memo(FeedDetailsIngredient);
