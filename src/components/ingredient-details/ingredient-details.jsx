a3962b78d1365d72b73da5511f238062ad215b38
theverybusybee <v_antonchik@mail.ru>
2022-10-27 09:03:42 +0300

    IngredientDetails component refactored

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,7 +1,7 @@
 import detailsStyles from "./ingredient-details.module.css";
 import { memo, useEffect, useState } from "react";
-import { fetchIngredients } from "../../utils/fetchOrderData";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function IngredientDetails() {
   const [ingredient, setIngredient] = useState({
@@ -13,65 +13,66 @@ function IngredientDetails() {
     carbohydrates: "",
   });
 
-  const { id } = useParams();
-  const { image, name, calories, proteins, fat, carbohydrates } = ingredient;
+  const allIngredients = useSelector(
+    (state) => state.apiDataReducer.allIngredients
+  );
 
-  const get = async () => {
-    const data = await fetchIngredients().then((data) => data);
-    if (data.success) {
-      const currentIngredient = data.data.find((el) => el._id === id);
-      if (!!currentIngredient) {
+  const { id } = useParams();
+  useEffect(() => {
+    if (allIngredients.length) {
+      const ingredient = allIngredients.find((el) => el._id === id);
+      if (ingredient) {
         setIngredient({
-          image: currentIngredient.image_large,
-          name: currentIngredient.name,
-          calories: currentIngredient.calories,
-          proteins: currentIngredient.proteins,
-          fat: currentIngredient.fat,
-          carbohydrates: currentIngredient.carbohydrates,
+          image: ingredient.image_large,
+          name: ingredient.name,
+          calories: ingredient.calories,
+          proteins: ingredient.proteins,
+          fat: ingredient.fat,
+          carbohydrates: ingredient.carbohydrates,
         });
       }
     }
-  };
+  }, [allIngredients]);
 
-  useEffect(() => {
-    get();
-  }, [id]);
+  const { image, name, calories, proteins, fat, carbohydrates } = ingredient;
 
   return (
-    <div className={detailsStyles.main}>
-      <h2 className={`${detailsStyles.title} text text_type_main-large`}>
-        Детали ингредиента
-      </h2>
-      <figure className={detailsStyles.imgContainer}>
-        <img className={detailsStyles.img} src={image} alt={name} />
-        <figcaption
-          className={`${detailsStyles.caption} text text_type_main-medium`}
-        >
-          {name}
-        </figcaption>
-      </figure>
+    image && (
+      <div className={detailsStyles.main}>
+        <h2 className={`${detailsStyles.title} text text_type_main-large`}>
+          Детали ингредиента
+        </h2>
+        <figure className={detailsStyles.imgContainer}>
+          <img className={detailsStyles.img} src={image} alt={name} />
+          <figcaption
+            className={`${detailsStyles.caption} text text_type_main-medium`}
+          >
+            {name}
+          </figcaption>
+        </figure>
 
-      <table
-        className={`${detailsStyles.nutrValTable} text text_type_main-small text_color_inactive`}
-      >
-        <thead>
-          <tr>
-            <td className={detailsStyles.nutrValCell}>Калории,ккал</td>
-            <td>Белки, г</td>
-            <td>Жиры, г</td>
-            <td>Углеводы, г</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{calories}</td>
-            <td>{proteins}</td>
-            <td>{fat}</td>
-            <td>{carbohydrates}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+        <table
+          className={`${detailsStyles.nutrValTable} text text_type_main-small text_color_inactive`}
+        >
+          <thead>
+            <tr>
+              <td className={detailsStyles.nutrValCell}>Калории,ккал</td>
+              <td>Белки, г</td>
+              <td>Жиры, г</td>
+              <td>Углеводы, г</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{calories}</td>
+              <td>{proteins}</td>
+              <td>{fat}</td>
+              <td>{carbohydrates}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
   );
 }
 
