e2fb9acd3e7e7a2c8449d0f689e08e4c4f30af84
theverybusybee <v_antonchik@mail.ru>
2022-10-28 15:29:26 +0300

    websocket connection request added to component

--- src/pages/order-feed-page/order-feed-page.jsx
+++ src/pages/order-feed-page/order-feed-page.jsx
@@ -1,10 +1,23 @@
 import feedPageStyles from "./order-feed-page.module.css";
 import OrderFeedStats from "../../components/order-feed-stats/order-feed-stats";
 import OrderFeedOrders from "../../components/order-feed-orders/order-feed-orders";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { WS_CONNECTION_START } from "../../services/actions/ws-actions";
 
 function OrderFeedPage() {
-  const allOrders = useSelector((state) => state.feedDataReducer.allOrders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: WS_CONNECTION_START,
+      payload: {
+        add: "/all",
+      },
+    })
+  });
+
+  const allOrders = useSelector((state) => state.wsReducer.allOrders);
   return (
     !!allOrders && (
       <div className={feedPageStyles.main}>
