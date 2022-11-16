2636c307326a14c1bc1635f4ee86855785e3e89e
theverybusybee <v_antonchik@mail.ru>
2022-11-16 15:02:10 +0300

    Updates

--- src/services/actions/tab.js
+++
@@ -1,3 +0,0 @@
-export const TAB_SWITCH = "TAB_SWITCH";
-
-export const TAB_NAME = { BUN: "Булки", SAUCE: "Соусы", STUFFING: "Начинки" };
--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const baseUrl = "https://norma.nomoreparties.space/api";
-export const checkResponce = (res) => {
-  if (res.ok) {
-    return res.json();
-  }
-  return Promise.reject(`Ошибка ${res.status}`);
-};
-
-export const modalsRoot = document.querySelector('#react-modals');
